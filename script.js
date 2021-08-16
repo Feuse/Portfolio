83222ac38a90f998163389337c5967671d0dba46
Roman Sterlin <Feuse132@gmail.com>
2021-08-16 22:32:31 +0300

    added null check for slider

--- script.js
+++ script.js
@@ -57,7 +57,8 @@ if (n < 1) {slideIndex = slides.length}
 for (i = 0; i < slides.length; i++) {
 slides[i].style.display = "none";
 }
-
-slides[slideIndex-1].style.display = "block";
+if(slides[slideIndex-1]){
+  slides[slideIndex-1].style.display = "block";
+}
 
 }
